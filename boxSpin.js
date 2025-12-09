2c0b3581f0d9c9365725b713625efb015b06b43f
Ls6046 <yh7853@pleasantonusd.net>
2025-12-09 14:06:22 -0800

    Update boxSpin.js

--- boxSpin.js
+++ boxSpin.js
@@ -6,11 +6,4 @@ AFRAME.registerComponent('box-spin', {
     rotation.y = (rotation.y + BOX_SPEED) % 360
     this.el.setAttribute('rotation', rotation)
   }
-    <a-entity box-spin
-                id="box" 
-                geometry="primitive:box"
-                position="0 3 -5"
-                material="color:blue;
-                          src: {Your-URL}">
-      </a-entity>
 });
