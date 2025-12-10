61aa5040d77025cd8fb35f3439f887257a4f6a09
shimyshake1 <ls6046@pleasantonusd.net>
2025-12-10 15:18:29 -0800

    Update notlookingAtBox.js

--- notlookingAtBox.js
+++ notlookingAtBox.js
@@ -1,1 +1,17 @@
+/* global AFRAME, BOX_NOT_SELECTED_COLOR */
 
+AFRAME.registerComponent('not-looking-at-box', {
+
+ dependencies: ['raycaster'],
+
+ init: function () {
+   this.el.addEventListener('raycaster-intersected-cleared', this.intersected.bind(this))
+ },
+  
+ intersectedcleared: function () {
+   let material = this.el.getAttribute('material')
+   material.color = BOX_NOT_SELECTED_COLOR
+   this.el.setAttribute('material', material)
+ }
+  
+});
