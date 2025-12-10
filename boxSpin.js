961e3a7449e18650d453998a463376617739aa2c
shimyshake1 <ls6046@pleasantonusd.net>
2025-12-10 14:51:32 -0800

    Update boxSpin.js

--- boxSpin.js
+++ boxSpin.js
@@ -4,6 +4,8 @@ AFRAME.registerComponent('box-spin', {
   tick: function (time, delta) {
     let rotation = this.el.getAttribute('rotation')
     rotation.y = (rotation.y + BOX_SPEED) % 360
+    rotation.x = (rotation.x + BOX_SPEED) % 360
+    rotation.xz= (rotation.z + BOX_SPEED) % 360
     this.el.setAttribute('rotation', rotation)
   }
 });
